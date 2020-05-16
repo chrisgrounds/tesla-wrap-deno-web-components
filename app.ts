cc40e6045efffdfb656caff8e258694a0a9c82ab
Chris Grounds <chrisbacon2009@hotmail.com>
2020-05-16 12:38:18 +0100

    Init

---
+++ app.ts
@@ -0,0 +1,27 @@
+import { Drash } from "https://deno.land/x/drash@v1.0.0/mod.ts";
+import { readFileStrSync } from "https://deno.land/std/fs/mod.ts"
+
+class HomeResource extends Drash.Http.Resource {
+  static paths = ["/"];
+  public GET() {
+    this.response.body = readFileStrSync("index.html");
+    return this.response;
+  }
+}
+
+const server = new Drash.Http.Server({
+  directory: "/Users/baconc01/tesla-deno-web-components",
+  response_output: "text/html",
+  resources: [HomeResource],
+  static_paths: ["/public"]
+});
+
+const HOSTNAME = "localhost"
+const PORT = 8080
+
+server.run({
+  hostname: HOSTNAME,
+  port: PORT
+});
+
+console.log(`Server listening: http://${HOSTNAME}:${PORT}`);
